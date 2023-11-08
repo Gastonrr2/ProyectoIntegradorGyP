2fcca44fb0c2231bee41b5bb8c8f42aba1594218
Gastonrr2 <140563105+Gastonrr2@users.noreply.github.com>
2023-11-08 19:51:02 -0300

    cambios 8/11/2023

--- ProyectoInt/app.js
+++ ProyectoInt/app.js
@@ -1,6 +1,10 @@
+
+
 const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
     const mobileNav = document.querySelector('.mobile-nav');
 
     mobileMenuIcon.addEventListener('click', () => {
         mobileNav.classList.toggle('active');
     });
+
+    
