b17a6297671567d0bc1a8c43dabc2c022c767b83
Gastonrr2 <140563105+Gastonrr2@users.noreply.github.com>
2023-11-07 22:10:40 -0300

    7/11

---
+++ ProyectoInt/app.js
@@ -0,0 +1,6 @@
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+    const mobileNav = document.querySelector('.mobile-nav');
+
+    mobileMenuIcon.addEventListener('click', () => {
+        mobileNav.classList.toggle('active');
+    });
