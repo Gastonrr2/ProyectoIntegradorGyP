a6d455f5f725489632ead07c0856dd422005ef25
Gastonrr2 <140563105+Gastonrr2@users.noreply.github.com>
2023-11-08 23:40:54 -0300

    dasdasdasd

--- ProyectoInt/app.js
+++ ProyectoInt/app.js
@@ -1,15 +1,21 @@
-
+console.log("inicio");
 const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
 const mobileNav = document.querySelector(".mobile-nav");
 const mobileLogo =document.querySelector(".mobile-logo")
+const modal = document.getElementById("form_pop");
 
     mobileMenuIcon.addEventListener("click", () => {
         mobileNav.classList.toggle("active");
     });
 
-
-
-  
+    document.getElementById("abrirModal").addEventListener("click", function () {
+      document.getElementById("form_pop").style.display = "block";
+      console.log("hola");
+    });
+    
+    document.getElementById("cerrarModal").addEventListener("click", function () {
+      document.getElementById("form_pop").style.display = "none";
+    });
     
 
 
